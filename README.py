636f886ab6f70453dfb2040d2fb46943b1a77f93
Felipe Nascimento de Souza <153876497+souzalipe@users.noreply.github.com>
2025-09-10 10:57:51 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,2 +1,7 @@
-BB CIN
-BB BNC AÇÕES NOSSA CAIXA NOSSO CLUBE DE INVESTIMENTO
+NameError: name 'empty' is not defined
+Traceback:
+File "C:\Users\T1092497\OneDrive - Banco do Brasil S.A\Documentos\Projetos\filtragem\app.py", line 696, in <module>
+    df_controle_fora = filtrar_controle_por_situacao(df_controle_fora)
+File "C:\Users\T1092497\OneDrive - Banco do Brasil S.A\Documentos\Projetos\filtragem\app.py", line 323, in filtrar_controle_por_situacao
+    if df is None or df is empty:
+      
